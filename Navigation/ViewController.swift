3e395be9c199aa67c70283e5f348e735707cdc50
Sanek <sashaloplop@mail.ru>
2023-05-21 16:15:21 +0300

    Initial commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Денис Кузьминов on 08.05.2023.
+//
+
+import UIKit
+
+
+
+
+class ViewController: UIViewController {
+    struct Post{
+        var title: String
+    }
+    var post = Post(title: "Posts")
+    private lazy var actionButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Перейти", for: .normal)
+        button.setTitleColor(.systemMint, for: .normal)
+        
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.addSubview(actionButton)
+        
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            actionButton.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            actionButton.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            actionButton.centerYAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.centerYAnchor),
+            
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        
+        actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+    }
+    
+    @objc func buttonPressed(_ sender: UIButton){
+        let postViewController = PostViewController()
+        
+        postViewController.modalTransitionStyle = .coverVertical
+        postViewController.modalPresentationStyle = .fullScreen
+        
+        present(postViewController, animated: true)
+    }
+    
+    
+}
+
