220553a48379261582d234f729cdcd97987ce539
Sanek <sashaloplop@mail.ru>
2023-06-16 22:59:03 +0300

    Second

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -18,6 +18,25 @@ class LogInViewController: UIViewController {
         return logo
     }()
 
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.backgroundColor = .white
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.layer.borderColor = UIColor.lightGray.cgColor
+        stack.layer.borderWidth = 0.5
+        stack.layer.cornerRadius = 10
+        
+        
+        return stack
+    }()
+    
+    private lazy var line: UIView = {
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.backgroundColor = .lightGray
+        
+        return line
+    }()
     
     private lazy var emailFeed: UITextField = {
         let feed = UITextField()
@@ -25,9 +44,6 @@ class LogInViewController: UIViewController {
         feed.placeholder = " Email or phone"
         feed.backgroundColor = .systemGray6
         feed.textColor = .black
-        feed.layer.borderColor = UIColor.lightGray.cgColor
-        feed.layer.borderWidth = 0.5
-        feed.layer.cornerRadius = 10
         feed.font = UIFont.systemFont(ofSize: 16)
         feed.autocapitalizationType = .none
         feed.keyboardType = UIKeyboardType.default
@@ -45,9 +61,6 @@ class LogInViewController: UIViewController {
         feed.placeholder = " Password"
         feed.backgroundColor = .systemGray6
         feed.textColor = .black
-        feed.layer.borderColor = UIColor.lightGray.cgColor
-        feed.layer.borderWidth = 0.5
-        feed.layer.cornerRadius = 10
         feed.font = UIFont.systemFont(ofSize: 16)
         feed.autocapitalizationType = .none
         feed.isSecureTextEntry = true
@@ -77,16 +90,26 @@ class LogInViewController: UIViewController {
     
     private lazy var scrollView: UIScrollView = {
         let view = UIScrollView()
+        
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.showsVerticalScrollIndicator = true
+        view.showsVerticalScrollIndicator = false
         view.showsHorizontalScrollIndicator = false
         view.backgroundColor = .white
-        view.center = view.center
-        //view.contentSize = CGSize(width: 1000, height: 1000)
 
+        
+        
         return view
     }()
     
+    private lazy var contenView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return view
+        
+    }()
+//-------------------------------------------func----------------------------------------------------------
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -103,26 +126,26 @@ class LogInViewController: UIViewController {
         tabBarController?.tabBar.isHidden = true
         setupKeyboardObservers()
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        //removeKeyboard()
-    }
     
     private func addSub(){
-       
         
         view.addSubview(scrollView)
-        scrollView.addSubview(vkLogo)
-        scrollView.addSubview(emailFeed)
-        scrollView.addSubview(passworFeed)
-        scrollView.addSubview(logButton)
         
+        
+        contenView.addSubview(stackView)
+        stackView.addSubview(emailFeed)
+        stackView.addSubview(passworFeed)
+        stackView.addSubview(line)
+        contenView.addSubview(vkLogo)
+        contenView.addSubview(logButton)
+        scrollView.addSubview(contenView)
+        
+
     }
 //---------------------------------------------setUp-------------------------------------------------------
     
     private func setUp(){
-        
+
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
             
@@ -130,49 +153,62 @@ class LogInViewController: UIViewController {
             scrollView.leftAnchor.constraint(equalTo: safeArea.leftAnchor),
             scrollView.rightAnchor.constraint(equalTo: safeArea.rightAnchor),
             scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-
+            
+            contenView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contenView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            contenView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+            contenView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+            contenView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
             
             vkLogo.heightAnchor.constraint(equalToConstant: 100),
             vkLogo.widthAnchor.constraint(equalToConstant: 100),
-            vkLogo.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 120),
-            vkLogo.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            vkLogo.topAnchor.constraint(equalTo: contenView.topAnchor, constant: 120),
+            vkLogo.centerXAnchor.constraint(equalTo: contenView.centerXAnchor),
             
-            
-            emailFeed.topAnchor.constraint(equalTo: vkLogo.bottomAnchor,constant: 120),
+            stackView.topAnchor.constraint(equalTo: vkLogo.bottomAnchor, constant: 120),
+            stackView.rightAnchor.constraint(equalTo: contenView.rightAnchor, constant: -16),
+            stackView.leftAnchor.constraint(equalTo: contenView.leftAnchor, constant: 16),
+            stackView.heightAnchor.constraint(equalToConstant: 100),
+
+            emailFeed.topAnchor.constraint(equalTo: stackView.topAnchor),
+            emailFeed.rightAnchor.constraint(equalTo: stackView.rightAnchor),
+            emailFeed.leftAnchor.constraint(equalTo: stackView.leftAnchor),
             emailFeed.heightAnchor.constraint(equalToConstant: 50),
-            emailFeed.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 16),
-            emailFeed.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: 380),
-
+            
             passworFeed.topAnchor.constraint(equalTo: emailFeed.bottomAnchor),
+            passworFeed.rightAnchor.constraint(equalTo: stackView.rightAnchor),
+            passworFeed.leftAnchor.constraint(equalTo: stackView.leftAnchor),
             passworFeed.heightAnchor.constraint(equalToConstant: 50),
-            passworFeed.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 16),
-            passworFeed.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: 380),
-
+            
+            line.heightAnchor.constraint(equalToConstant: 0.5),
+            line.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+            line.rightAnchor.constraint(equalTo: stackView.rightAnchor),
+            line.leftAnchor.constraint(equalTo: stackView.leftAnchor),
+            
             logButton.topAnchor.constraint(equalTo: passworFeed.bottomAnchor, constant: 16),
             logButton.heightAnchor.constraint(equalToConstant: 50),
-            logButton.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 16),
-            logButton.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: 380),
+            logButton.leftAnchor.constraint(equalTo: contenView.leftAnchor, constant: 16),
+            logButton.rightAnchor.constraint(equalTo: contenView.rightAnchor, constant: -16),
+
 
 
         ])
     }
-//----------------------------------------------------------------------------------------------------- ---
+//---------------------------------------------------------------------------------------------------------
+   
     private func setupKeyboardObservers(){
         
         let noficiationCenter = NotificationCenter.default
         
-        noficiationCenter.addObserver(self, selector: #selector(self.willShowKeyboard(_ :)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        noficiationCenter.addObserver(self, selector: #selector(willShowKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
         
-        noficiationCenter.addObserver(self, selector: #selector(self.willHIdeKeyboard(_ :)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        noficiationCenter.addObserver(self, selector: #selector(willHIdeKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    private func removeKeyboard(){
-        let notificion = NotificationCenter.default
-        notificion.removeObserver(self)
-    }
+    
+    
     @objc func willShowKeyboard(_ notification: NSNotification){
         let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height
         scrollView.contentInset.bottom += keyboardHeight ?? 0.0
-        
     }
     
     @objc func willHIdeKeyboard(_ notification: NSNotification){
@@ -183,6 +219,7 @@ class LogInViewController: UIViewController {
         let profileViewController = ProfileViewController()
         navigationController?.pushViewController(profileViewController, animated: true)
     }
+    
 }
 
 extension LogInViewController: UITextFieldDelegate{
